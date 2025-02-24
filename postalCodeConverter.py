4c37803724b5c76f9652f54eb14929bacbc28504
alexGO11 <algome26@ucm.es>
2025-02-24 22:45:23 +0100

    Firsts new versions of the next step

--- postalCodeConverter.py
+++ postalCodeConverter.py
@@ -2,10 +2,7 @@ import pandas as pd
 import time
 import tqdm
 
-raw = pd.read_csv('20241031_reporting.csv')
-df_postal = pd.read_csv('postal_codes.csv')
-
-pc_serie = pd.Series(df_postal.iloc[:, 0])
+raw = pd.read_csv('data/new_data.csv')
 
 df_groupby = raw.groupby(['Postal code']).agg(
     n_positives=('Value test', lambda x: (x == 'Positivo').sum()),
@@ -13,11 +10,6 @@ df_groupby = raw.groupby(['Postal code']).agg(
 ).reset_index()
 
 df_groupby['Postal code'] = df_groupby['Postal code'].astype(int)   
+df_groupby["Postal code"] = df_groupby["Postal code"].astype(str).str.zfill(5)
 
-df_groupby.to_csv('grouped_pc.csv', index=False)
-
-pbar = tqdm.trange(100,desc="Exportando...")
-for i in range(10):
-    pbar.update(n=10)
-    time.sleep(0.1)
-pbar.close()
+df_groupby.to_csv('data/grouped_pc.csv', index=False)
